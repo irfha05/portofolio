d70cfb99cbed39fe544adebd01d700509a4c60a0
Irfha <irfha.naqis26@gmail.com>
2025-01-06 23:33:32 +0800

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -52,3 +52,73 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
 } else {
   darkToggle.checked = false;
 }
+document.addEventListener("DOMContentLoaded", () => {
+  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  let index = 0;
+  let textIndex = 0;
+  const rotate = document.getElementById("rotateText");
+  let isDeleting = false;
+  const typingSpeed = 100;
+  const deletingSpeed = 50;
+  const delayBetweenWords = 1500;
+
+  function typeEffect() {
+    const currentText = toRotate[index];
+
+    if (isDeleting) {
+      rotate.textContent = currentText.substring(0, textIndex--);
+      if (textIndex < 0) {
+        isDeleting = false;
+        index = (index + 1) % toRotate.length;
+        setTimeout(typeEffect, typingSpeed);
+      } else {
+        setTimeout(typeEffect, deletingSpeed);
+      }
+    } else {
+      rotate.textContent = currentText.substring(0, textIndex++);
+      if (textIndex > currentText.length) {
+        isDeleting = true;
+        setTimeout(typeEffect, delayBetweenWords);
+      } else {
+        setTimeout(typeEffect, typingSpeed);
+      }
+    }
+  }
+
+  typeEffect();
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const toRotate = ["Irfha Najla Qisti ar", "D1A240058"];
+  let index = 0;
+  let textIndex = 0;
+  const rotate = document.getElementById("rotateTexts");
+  let isDeleting = false;
+  const typingSpeed = 100;
+  const deletingSpeed = 50;
+  const delayBetweenWords = 1500;
+
+  function typeEffect() {
+    const currentText = toRotate[index];
+
+    if (isDeleting) {
+      rotate.textContent = currentText.substring(0, textIndex--);
+      if (textIndex < 0) {
+        isDeleting = false;
+        index = (index + 1) % toRotate.length;
+        setTimeout(typeEffect, typingSpeed);
+      } else {
+        setTimeout(typeEffect, deletingSpeed);
+      }
+    } else {
+      rotate.textContent = currentText.substring(0, textIndex++);
+      if (textIndex > currentText.length) {
+        isDeleting = true;
+        setTimeout(typeEffect, delayBetweenWords);
+      } else {
+        setTimeout(typeEffect, typingSpeed);
+      }
+    }
+  }
+
+  typeEffect();
+});
